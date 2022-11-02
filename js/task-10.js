262daf17767c9a6121befbafd531417026bc607a
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-11-02 18:02:28 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const boxes = document.getElementById("boxes");
+create.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  const amount = document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  const basicSize = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i += 1) {
+    const size = basicSize + i * 10;
+    const div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
